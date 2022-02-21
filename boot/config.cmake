45736e68f30ae590248307c5792b24423fbce822
yyangoO <yyangoO@outlook.com>
2022-02-21 16:32:36 +0800

    update cmake configuration of bootloader

--- boot/config.cmake
+++ boot/config.cmake
@@ -1,12 +1,11 @@
 cmake_minimum_required(VERSION 3.14)
 
-set(INIT_PATH       "${BOOTLOADER_PATH}")
+set(INIT_PATH "${BOOTLOADER_PATH}")
 
 file(
     GLOB
         tmpfiles
         "${INIT_PATH}/*.c"
-        "${INIT_PATH}/*.S"
-)
+        "${INIT_PATH}/*.S")
 
 list(APPEND files ${tmpfiles})
