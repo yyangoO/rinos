a19780f1ede727476ab15567ce53602aa4a1ffd8
yyangoO <yyangoO@outlook.com>
2022-02-21 20:37:38 +0800

    update os I/O

--- kernel/common/kprint.h
+++ kernel/common/kprint.h
@@ -1,25 +1,32 @@
-/*
- * Copyright (c) 2020 Institute of Parallel And Distributed Systems (IPADS), Shanghai Jiao Tong University (SJTU)
- * OS-Lab-2020 (i.e., ChCore) is licensed under the Mulan PSL v1.
- * You can use this software according to the terms and conditions of the Mulan PSL v1.
- * You may obtain a copy of Mulan PSL v1 at:
- *   http://license.coscl.org.cn/MulanPSL
- *   THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
- *   IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
- *   PURPOSE.
- *   See the Mulan PSL v1 for more details.
+/**
+ * @file                    kprint.h
+ * @author                  yyang yyangoO@outlook.com & sjtu_yangyang@sjtu.edu.cn
+ * @version                 1.0.0
+ * @date                    24-APR-2021
+ * @section                 description
+ *                          basic I/O function
+ * @section                 platforn
+ * -                        Raspberry Pi 3b
+ * @section                 logs
+ * <table>
+ * <tr><th>date             <th>version     <th>author      <th>description     </tr>
+ * <tr><td>2021.04.24       <td>1.0.0       <td>yyang       <td>initial version </tr>
+ * </tr>
  */
 
-#pragma once
+#ifndef KPRINT_H
+#define KPRINT_H
+
 
 #include "printk.h"
+
 
 #define WARNING 0
 #define INFO 1
 #define DEBUG 2
 
-/* LOG_LEVEL is INFO by default */
 
+// LOG_LEVEL is INFO by default
 #if LOG_LEVEL >= WARNING
 #define kwarn(fmt, ...) printk("[WARN] "fmt, ##__VA_ARGS__)
 #else
@@ -37,3 +44,6 @@
 #else
 #define kdebug(fmt, ...)
 #endif
+
+
+#endif // KPRINT_H
