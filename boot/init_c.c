d430ea4b9d092fb6aa9c2f6f14b439a052f0458b
yyangoO <yyangoO@outlook.com>
2022-02-21 18:20:02 +0800

    update bootloader

--- boot/init_c.c
+++ boot/init_c.c
@@ -15,7 +15,7 @@
  */
 
 
-#define PLAT_CPU_NUMBER		4           ///< armv8 aarch64 cpu kernel numbers
+#define PLAT_CPU_NUMBER  4              ///< armv8 aarch64 cpu kernel numbers
 #define INIT_STACK_SIZE     0x1000      ///< initial stack size
 char boot_cpu_stack[PLAT_CPU_NUMBER][INIT_STACK_SIZE] __attribute__((__aligned__(16)));
 
@@ -49,47 +49,50 @@ volatile u64 clear_bss_flag = NOT_BSS;
  * This function initialize the UART function. 
  */
 void early_uart_init(void);
+
 /**
  * @brief send string from UART
  * This function sends string by using UART.
  */
 void uart_send_string(char *);
+
 /**
  * @brief clear bss area
  * This function clear the bss area.
  */
 static void clear_bss(void)
 {
-	u64 bss_start_addr;
-	u64 bss_end_addr;
-	u64 i;
+    u64 bss_start_addr;
+    u64 bss_end_addr;
+    u64 i;
 
-	bss_start_addr = (u64) & _bss_start;
-	bss_end_addr = (u64) & _bss_end;
+    bss_start_addr = (u64) & _bss_start;
+    bss_end_addr = (u64) & _bss_end;
 
-	for (i = bss_start_addr; i < bss_end_addr; ++i)
+    for (i = bss_start_addr; i < bss_end_addr; ++i)
     {
-		*(char *)i = 0;
+        *(char *)i = 0;
     }
-	clear_bss_flag = 0;
+    clear_bss_flag = 0;
 }
+
 /**
  * @brief initializate bootloader
  * This function initialize bootloader.
  */
 void init_c(void)
 {
-	clear_bss();                                // clear the bss area for the kernel image
+    clear_bss();                                // clear the bss area for the kernel image
 
-	early_uart_init();                          // initialize UART before enable MMU
-	uart_send_string("boot: init_c\r\n");
+    early_uart_init();                          // initialize UART before enable MMU
+    uart_send_string("[BOOT] initialize early UART\r\n");
 
-	init_boot_pt();                             // initialize boot page table
+    init_boot_pt();                             // initialize boot page table
     uart_send_string("[BOOT] install boot page table\r\n");
 
-	el1_mmu_activate();                         // enable MMU
-	uart_send_string("[BOOT] enable el1 MMU\r\n");
+    el1_mmu_activate();                         // enable MMU
+    uart_send_string("[BOOT] enable el1 MMU\r\n");
 
-	start_kernel(secondary_boot_flag);          // call kernel main
-    uart_send_string("[BOOT] Jump to kernel main\r\n");
+    start_kernel(secondary_boot_flag);          // call kernel main
+    uart_send_string("[BOOT] jump to kernel main\r\n");
 }
