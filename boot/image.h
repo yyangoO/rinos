0d6797bc3e44030b910dd3a5e225e12c52f7922f
yyangoO <yyangoO@outlook.com>
2022-02-21 20:28:34 +0800

    update bootloader

--- boot/image.h
+++ boot/image.h
@@ -1,4 +1,20 @@
-#pragma once
+/**
+ * @file                    image.h
+ * @author                  yyang yyangoO@outlook.com & sjtu_yangyang@sjtu.edu.cn
+ * @version                 1.0.0
+ * @date                    24-APR-2021
+ * @section                 description
+ *                          header of os's image
+ * @section                 platforn
+ * -                        Raspberry Pi 3b
+ * @section                 logs
+ * <table>
+ * <tr><th>date             <th>version     <th>author      <th>description     </tr>
+ * <tr><td>2021.04.24       <td>1.0.0       <td>yyang       <td>initial version </tr>
+ * </tr>
+ */
+#ifndef IMAGE_H
+#define IMAGE_H
 
 
 #define SZ_16K          0x4000                  ///< aligned size is 16K
@@ -6,3 +22,5 @@
 
 #define KERNEL_VADDR    0xffffff0000000000      ///< kernel's viturl address
 #define TEXT_OFFSET     0x80000                 ///< the offset of data in ELF
+
+#endif // IMAGE_H
