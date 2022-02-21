68f1091c6e6a8470afd3a7713df663d2cd509500
yyangoO <yyangoO@outlook.com>
2022-02-21 18:04:57 +0800

    update image parameter

--- boot/image.h
+++ boot/image.h
@@ -1,7 +1,8 @@
 #pragma once
 
-#define SZ_16K			0x4000
-#define SZ_64K                  0x10000
 
-#define KERNEL_VADDR		0xffffff0000000000
-#define TEXT_OFFSET		0x80000
+#define SZ_16K          0x4000                  ///< aligned size is 16K
+#define SZ_64K          0x10000                 ///< aligned size is 64k
+
+#define KERNEL_VADDR    0xffffff0000000000      ///< kernel's viturl address
+#define TEXT_OFFSET     0x80000                 ///< the offset of data in ELF
